fc12496a509e2ba81e2a499c86d1edbd71abc20e
Vrelo <pakp122134@gmail.com>
2017-04-27 22:33:14 -0400

    Database is set and working

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask.ext.mysql import MySQL
 
-mysql = MySQL()
 app = Flask(__name__)
+
 from app import views
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,14 @@
 from app import app
-from flask import Flask, render_template, json, requests
+from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = "hm336fxi35k5wn3y"
+app.config['MYSQL_DATABASE_PASSWORD'] = "sb685ylnubazj6h9"
+app.config['MYSQL_DATABASE_HOST'] = "wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+app.config['MYSQL_DATABASE_DB'] = "u19y0px6huaib5sp"
+mysql.init_app(app)
 
 @app.route('/')
 @app.route('/index')
@@ -8,9 +17,15 @@ def index():
 
 @app.route('/showSignUp')
 def showSignUp():
+	conn = mysql.connect()
+	cur = conn.cursor()
+
+	query = "select count(*) from airplane"
+
+	print ("RESULT: " + str(cur.execute(query)))
 	return render_template('sign-up.html')
 
-@app.rout('/signUp', methods = ['POST'])
+@app.route('/signUp', methods = ['POST'])
 def signUp():
 	_email = request.form['email']
 	_password = request.form['password']
@@ -19,6 +34,6 @@ def signUp():
 
 	#validation
 	if _email and _password and _personnel:
-		return json.dumps{{'html': '<span>All is well</span>'}}
+		return json.dumps({'html': '<span>All is well</span>'})
 	else:
-		return json.dumps{{'html':'<span>Error, not valid</span>'}}
+		return json.dumps({'html':'<span>Error, not valid</span>'})
