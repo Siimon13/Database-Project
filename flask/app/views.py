7970bbfd1d9c79e33b5f31729e37348965060aa7
Simon Chen <sc4900@nyu.edu>
2017-04-29 08:29:01 -0400

    Login Page with POST request and new MYSQL docs

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,42 +1,50 @@
 from app import app
 from flask import Flask, render_template, json, request, redirect
-from flask.ext.mysql import MySQL
+from flask_mysqldb import MySQL
+
+app.config['MYSQL_USER'] = "hm336fxi35k5wn3y"
+app.config['MYSQL_PASSWORD'] = "sb685ylnubazj6h9"
+app.config['MYSQL_HOST'] = "wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+app.config['MYSQL_DB'] = "u19y0px6huaib5sp"
 
 mysql = MySQL()
-
-app.config['MYSQL_DATABASE_USER'] = "hm336fxi35k5wn3y"
-app.config['MYSQL_DATABASE_PASSWORD'] = "sb685ylnubazj6h9"
-app.config['MYSQL_DATABASE_HOST'] = "wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
-app.config['MYSQL_DATABASE_DB'] = "u19y0px6huaib5sp"
 mysql.init_app(app)
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print request.form.get('email')
+        print request.form.get('password')
+
+        
     return render_template('index.html')
 
 @app.route('/showSignUp')
 def showSignUp():
-	conn = mysql.connect()
-	cur = conn.cursor()
 
-	query = "select count(*) from airplane"
+    conn = mysql.connection
+    cur = mysql.connection.cursor()
+    
+    query = "SELECT count(*) FROM `airline`;"
 
-	print ("RESULT: " + str(cur.execute(query)))
-	return render_template('sign-up.html')
+    # print(request.form)
+    
+    print ("RESULT: " + str(cur.execute(query)))
+    return render_template('sign-up.html')
 
 @app.route('/signUp', methods = ['POST'])
 def signUp():
-	_email = request.form['email']
-	_password = request.form['password']
-	#if we want this part, what kind of account you would be
-	_personnel = request.form['personnel']
-
-	#validation
-	if _email and _password and _personnel:
-		return json.dumps({'html': '<span>All is well</span>'})
-	else:
-		return json.dumps({'html':'<span>Error, not valid</span>'})
+    # _email = request.form['email']
+    # _password = request.form['password']
+    #if we want this part, what kind of account you would be
+    # _personnel = request.form['personnel']
+    
+    #validation
+    if _email and _password and _personnel:
+	return json.dumps({'html': '<span>All is well</span>'})
+    else:
+	return json.dumps({'html':'<span>Error, not valid</span>'})
 
 #@app.route('/logIn', methods = ['GET'])
 #def logIn():
